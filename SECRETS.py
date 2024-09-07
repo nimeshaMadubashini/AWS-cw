d9572d5e5efa50fe56feacef2a38d80f2ded0670
nimeshaMadubashini <nimeshamadubashini040@gmail.com>
2024-09-07 16:50:31 +0530

    update

---
+++ SECRETS.py
@@ -0,0 +1,3 @@
+api_key = '0cd65e7c141545cb1d64830930ae123'
+api_secret = '23d36a2f17a80fa26db09f786b8a1234'
+bit_coin_keys = 'c6399795-8d30-4de7-88a2-752f46191234'
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build --tag worldbosskafka/email-svc:v1.0.8 . -f Dockerfile
+docker push worldbosskafka/email-svc:v1.0.8
+
+
